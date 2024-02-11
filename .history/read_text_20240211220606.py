f4891d381446d528b7e1de917db4e40d1c18bb38
ishimweemmy <ishimweemmanuel2005@gmail.com>
2024-02-11 22:12:33 +0200

    [feat]: work done

---
+++ .history/read_text_20240211220606.py
@@ -0,0 +1,22 @@
+import cv2 
+import pytesseract 
+import numpy as np
+
+img = cv2.imread('meter.jpg') 
+
+# Convert the image to gray scale 
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+ 
+# Use Gaussian blur to remove noise 
+blur = cv2.GaussianBlur(gray, (5,5), 0) 
+bin_img = cv2.adaptiveThreshold(blur, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 31, 2)
+ 
+# Perform dilation and erosion to remove some noise 
+kernel = np.ones((1, 1), np.uint8) 
+img = cv2.dilate(bin_img, kernel, iterations=1) 
+img = cv2.erode(img, kernel, iterations=1) 
+
+# Use pytesseract to convert the image data to text 
+text = pytesseract.image_to_string(img) 
+
+print(text)
---
+++ .history/read_text_20240211221026.py
@@ -0,0 +1,17 @@
+import numpy as np
+import pytesseract
+import cv2
+
+image = cv2.imread('meter.jpg')
+
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+sharpen_kernel = np.array([[-1,-1,-1], [-1,9,-1], [-1,-1,-1]])
+
+sharpen = cv2.filter2D(gray, -1, sharpen_kernel)
+thresh = cv2.threshold(sharpen, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+
+data = pytesseract.image_to_string(thresh, lang='eng', config='--psm 3')
+
+measurement = ''.join(filter(str.isdigit, data))
+
+print("Extracted Numbers:", measurement)
